3c5eaf611f8d89f38b859f9458ed3176e215718c
Denba32 <84996620+Denba32@users.noreply.github.com>
2024-04-29 19:34:58 +0900

    fix : Update
    fix : Update

--- obj/Debug/net8.0/Sparta Dungeon.AssemblyInfo.cs
+++ obj/Debug/net8.0/Sparta Dungeon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sparta Dungeon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b4533d7ed13f3fd77930791f26aa292265d14c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ad4cc0982422dc738f6edf41c7723c432c5c933")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sparta Dungeon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sparta Dungeon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
