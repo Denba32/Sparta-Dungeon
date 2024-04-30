416a008d53bfb2fc528d87552bff86548322b118
Blacksnow1234 <167046482+Blacksnow1234@users.noreply.github.com>
2024-04-30 12:32:20 +0900

    feat : Add
    + Enemy Date and Enemy

---
+++ Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_Dungeon
+{
+    public class Enemy
+    {
+        public static List<EnemyData> Monster()
+        {
+            List<EnemyData> monster = new List<EnemyData>
+            {
+                new EnemyData("슬라임", 1, 2, 5),
+                new EnemyData("고블린", 3, 5, 10),
+                new EnemyData("스켈레톤", 8, 9, 5),
+                new EnemyData("게 허르 좁", 80, 1, 1),
+                new EnemyData("존 나르센", 99, 80, 70),
+                new EnemyData("단 데르기", 1, 1, 99)
+
+            };
+
+            return monster;
+        }
+    }
+}
---
+++ EnemyData.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta_Dungeon
+{
+    [System.Serializable]
+    public class EnemyData
+    {
+        [JsonProperty]
+        public string Name {  get; set; }
+        [JsonProperty]
+        public int Level { get; set; }
+        [JsonProperty]
+        public int Atk { get; set; }
+        [JsonProperty]
+        public int Hp { get; set; }
+
+        public EnemyData(string name, int level, int atk, int hp) 
+        {
+            Name = Name;
+            Level = level;
+            Atk = atk;
+            Hp = hp;
+        }
+
+    }
+}
